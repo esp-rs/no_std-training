4b0ebcd544d4ad83e8a928fbc692943c39160c59
bjoernQ <bjoern.quentin@mobile-j.de>
2023-09-06 16:59:16 +0200

    Remove WDT disabling

--- advanced/stack_overflow_detection/examples/stack_overflow_protection.rs
+++ advanced/stack_overflow_detection/examples/stack_overflow_protection.rs
@@ -8,32 +8,16 @@ use esp_backtrace as _;
 use esp_println::println;
 use hal::{
     assist_debug::DebugAssist,
-    clock::ClockControl,
     interrupt,
     peripherals::{self, Peripherals},
     prelude::*,
-    timer::TimerGroup,
-    Rtc,
 };
 
 #[entry]
 fn main() -> ! {
     let peripherals = Peripherals::take();
     let system = peripherals.SYSTEM.split();
-    let clocks = ClockControl::boot_defaults(system.clock_control).freeze();
     let mut peripheral_clock_control = system.peripheral_clock_control;
-
-    // Disable the RTC and TIMG watchdog timers
-    let mut rtc = Rtc::new(peripherals.RTC_CNTL);
-    let timer_group0 = TimerGroup::new(peripherals.TIMG0, &clocks, &mut peripheral_clock_control);
-    let mut wdt0 = timer_group0.wdt;
-    let timer_group1 = TimerGroup::new(peripherals.TIMG1, &clocks, &mut peripheral_clock_control);
-    let mut wdt1 = timer_group1.wdt;
-
-    rtc.swd.disable();
-    rtc.rwdt.disable();
-    wdt0.disable();
-    wdt1.disable();
 
     // get the debug assist driver
     let da = DebugAssist::new(peripherals.ASSIST_DEBUG, &mut peripheral_clock_control);
