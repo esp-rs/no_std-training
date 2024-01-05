f69f0546895336056468fde5dc459df637001d45
Sergio Gasquez Arcos <sergio.gasquez@gmail.com>
2024-01-05 10:08:29 +0100

    Add a defmt project (#33)
    * feat: Initial defmt chapter
    
    * feat: Add esp-backtrace information
    
    * feat: Remove .gitignore files
    
    * ci: Cover defmt project

---
+++ intro/defmt/examples/defmt.rs
@@ -0,0 +1,29 @@
+#![no_std]
+#![no_main]
+
+// ANCHOR: println_include
+use esp_backtrace as _;
+use esp_println as _;
+// ANCHOR_END: println_include
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    defmt::trace!("trace");
+    defmt::debug!("debug");
+    defmt::info!("info");
+    defmt::warn!("warn");
+    defmt::error!("error");
+
+    // panic!("Very useful panic message");
+
+    loop {
+        defmt::println!("Loop...");
+        delay.delay_ms(500u32);
+    }
+}
---
+++ intro/defmt/src/main.rs
@@ -0,0 +1,23 @@
+#![no_std]
+#![no_main]
+
+//  Build the `esp_println` and `esp_backtrace` libs
+
+use hal::{clock::ClockControl, peripherals::Peripherals, prelude::*, Delay};
+
+#[entry]
+fn main() -> ! {
+    let peripherals = Peripherals::take();
+    let system = peripherals.SYSTEM.split();
+    let clocks = ClockControl::max(system.clock_control).freeze();
+    let mut delay = Delay::new(&clocks);
+
+    // Print a log or a message using defmt
+
+    // Use a panic! macro to trigger a panic
+
+    loop {
+        defmt::println!("Loop...");
+        delay.delay_ms(500u32);
+    }
+}
